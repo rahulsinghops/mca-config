d184ae23fea4361b353e98a025565d46501f703a
kingr <kingr@DESKTOP-4872VOO>
2021-06-02 21:44:41 +0530

    setup for config server.

---
+++ mca-config/src/test/java/com/ipru/mca/mcaconfig/McaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipru.mca.mcaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McaConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
